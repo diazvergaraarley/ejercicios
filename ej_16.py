d45614e67ed41efa0faa309e4a53e80d5d86349b
diazvergaraarley <diazvergara.arley@gmail.com>
2026-03-16 20:32:09 -0500

    update

---
+++ ej_16.py
@@ -0,0 +1,17 @@
+edibles = {
+
+    "dogs food": 2500,
+    "cats food": 3200,
+    "fish food": 6000
+}
+toys = {
+    "bone": 2800,
+    "feathers": 3500,
+    "bubles": 4500
+}
+accesories = {
+    "neckless": 1500,
+    "bed": 6200,
+    "shark": 4200
+}
+categories =[edibles , toys, accesories]
